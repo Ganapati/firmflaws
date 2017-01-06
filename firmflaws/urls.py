110dc17eb6c46fd54477cbdadb86a6a4ea1d92c0
Ganapati <ganapati@kalkulators.org>
2017-01-06 13:23:36 +0100

    Fix patterns for django compatibility

--- firmflaws/urls.py
+++ firmflaws/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, patterns
+from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
 from django.conf import settings
